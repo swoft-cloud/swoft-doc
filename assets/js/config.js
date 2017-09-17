62b09523609341f94048db91b166b6ebbecfa0ac
inhere <in.798@qq.com>
2017-09-17 19:11:23 +0800

    update md site reader components

---
+++ assets/js/config.js
@@ -0,0 +1,27 @@
+  // page title = baseTile + content title
+// config.baseTitle = 'Go Web 编程'
+// config.siteName = '《Go Web 编程》'
+// config.siteDes = 'Go Web 编程的描述信息'
+// config.siteKey = 'go-web-dev'
+// config.logoUrl = '/md-site-reader/'
+
+// config.docProject = 'astaxie/build-web-application-with-golang'
+// //   // e.g https://raw.githubusercontent.com/{astaxie/build-web-application-with-golang}/master/{beanfactory.md}
+// config.dataUrl = 'https://raw.githubusercontent.com/astaxie/build-web-application-with-golang/master/zh/'
+// config.docUrl = 'https://github.com/astaxie/build-web-application-with-golang'
+// //   // e.g https://github.com/astaxie/build-web-application-with-golang/edit/master/http.md
+// config.editUrl = 'https://github.com/astaxie/build-web-application-with-golang/edit/master'
+
+// config.project = 'inhere/md-site-reader'
+// config.projectUrl = 'https://github.com/inhere/md-site-reader'
+// config.issueUrl = 'https://github.com/astaxie/build-web-application-with-golang/issues'
+
+// config.authorPage = 'https://github.com/astaxie'
+// config.authorName = 'astaxie'
+
+// config.theme = 'paper'
+// config.codeTheme = 'github'
+// config.catelogPage = 'SUMMARY.md'
+// config.defaultPage = 'README.md'
+// config.makeTOC = true
+// config.emptyData = 'No content to display!'
